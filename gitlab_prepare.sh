7a9b3d1e599e9719be8f511312b7dc8fecacff6e
Trung Phan <trung.phan@kitchenstories.de>
2017-05-04 15:20:22 +0000

    Added: tests

---
+++ gitlab_prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US:en
+export WORKON_HOME=/home/gitlab_ci_multi_runner/.virtualenvs
+mkdir -p $WORKON_HOME
+source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv ultron-client-env || true
+source /home/gitlab_ci_multi_runner/.virtualenvs/ultron-client-env/bin/activate
+pip install -r requirements.txt
